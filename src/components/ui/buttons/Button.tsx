98f10010a350636ad14775a4219135c09383518c
minyeong <minyk0512@gmail.com>
2025-04-01 17:06:00 +0900

    style: 버튼 텍스트 선택되지 않도록 수정 active 상태 폰트 색상 설정

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -20,6 +20,7 @@ export default function Button({ children, onClick, disabled = false, size, vari
         font-pretendard 
         font-semibold 
         letter-spacing-[-0.03em] 
+        select-none
         disabled:bg-neutral-300 
         disabled:text-neutral-600
       `}
@@ -31,7 +32,8 @@ export default function Button({ children, onClick, disabled = false, size, vari
 
 // TODO: 버튼 디자인시스템 확정 후 primary 제외 버튼 스타일들 추가
 const BUTTON_STYLES = {
-  'primary-large': 'bg-primary-300 rounded-[10px] text-neutral-1100 hover:text-neutral-100 active:bg-primary-400',
+  'primary-large':
+    'bg-primary-300 rounded-[10px] text-neutral-1100 hover:text-neutral-100 active:bg-primary-400 active:text-neutral-100',
   'primary-medium': 'bg-primary-300 rounded-[10px] text-neutral-100 active:bg-primary-400',
   'primary-small': 'bg-primary-300 rounded-[10px] text-neutral-100 active:bg-primary-400',
   'secondary-large': '',
